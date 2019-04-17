af5b3d6b773e0b5e06e972e958d7b398935d798a
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-04-17 15:39:36 +0200

    Première étape atteinte. J'ai eu un peu de mal sur `testEmptyInterned()` que Rémi va devoir expliquer !

--- src/main/java/fr/umlv/lexer/Lexer.java
+++ src/main/java/fr/umlv/lexer/Lexer.java
@@ -1,3 +1,21 @@
 package fr.umlv.lexer;
 
+import java.util.Objects;
+import java.util.Optional;
+
 // TODO
+public class Lexer<Type> {
+	
+	private static Lexer lexer;
+	
+	public static <Type> Lexer<Type> create() {
+		if(lexer==null)
+			lexer = new Lexer<>();
+		return lexer;
+	}
+
+	public Optional<Type> tryParse(String text) {
+		Objects.requireNonNull(text);
+		return Optional.empty();
+	}
+}
